6ac92e3676d92cf732df4d107451c01880b260c7
Henry Jonathan <henryjonathanchandra12@gmail.com>
2024-08-26 17:02:47 +0700

    modify card

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,31 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Card = ({ name, icon, bgColor, value, hasDetail, onClick }) => {
-  const [showTooltip, setShowTooltip] = useState(false);
-
-  return (
-    <div
-      className={`relative flex flex-col items-center rounded-lg bg-white shadow-lg px-4 py-4 lg:py-6 w-full transition-transform transform ${
-        hasDetail ? 'cursor-pointer hover:bg-gray-100 hover:scale-105' : ''
-      }`}
-      onClick={hasDetail ? onClick : undefined}
-      onMouseEnter={() => setShowTooltip(true)}
-      onMouseLeave={() => setShowTooltip(false)}
-    >
-      <div className='flex items-center space-x-2'>
-        <div className={`flex items-center justify-center rounded-full p-2 w-9 h-9 ${bgColor}`}>
-          {icon}
-        </div>
-        <div className='text-slate-800 font-bold text-base lg:text-xl'>{name}</div>
+const Card = ({ name, icon, bgColor, value, hasDetail, onClick }) => (
+  <div
+    className={`flex flex-col items-center rounded-lg bg-white shadow-lg px-4 py-4 lg:py-6 w-full transition-transform transform ${
+      hasDetail ? 'cursor-pointer hover:bg-gray-100 hover:scale-105' : ''
+    }`}
+    onClick={hasDetail ? onClick : undefined}
+  >
+    <div className='flex items-center space-x-2'>
+      <div className={`flex items-center justify-center rounded-full p-2 w-9 h-9 ${bgColor}`}>
+        {icon}
       </div>
-      <div className='mt-4 text-base lg:text-xl font-bold text-slate-700'>{value}</div>
-      {hasDetail && showTooltip && (
-        <div className="tooltip tooltip-visible">
-          Klik untuk melihat detail
-        </div>
-      )}
+      <div className='text-slate-800 font-bold text-base lg:text-xl'>{name}</div>
     </div>
-  );
-};
+    <div className='mt-4 text-base lg:text-xl font-bold text-slate-700'>{value}</div>
+    {hasDetail && (
+      <div className="absolute inset-0 pointer-events-none"></div>
+    )}
+  </div>
+);
 
 export default React.memo(Card);
